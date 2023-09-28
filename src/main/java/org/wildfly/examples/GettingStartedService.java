b358fc89892714abe371f7bed51eb15747e3dd34
Emmanuel Hugonnet <ehugonne@redhat.com>
2023-09-28 17:40:53 +0200

    Simple application for the cloud
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

---
+++ src/main/java/org/wildfly/examples/GettingStartedService.java
@@ -0,0 +1,11 @@
+package org.wildfly.examples;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GettingStartedService {
+
+    public String hello(String name) {
+        return String.format("Hello '%s'.", name);
+    }
+}
